e75db1b95a447db0ded2e852b51491418a4bf635
Kammenk <35629844+Kammenk@users.noreply.github.com>
2020-08-09 14:55:42 +0300

    adding google maps

--- app/src/main/java/com/example/brewmaster/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/brewmaster/ui/fragments/home/HomeFragment.kt
@@ -1,23 +1,35 @@
 package com.example.brewmaster.ui.fragments.home
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.brewmaster.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+
+import com.google.android.gms.maps.model.MarkerOptions
 
 
-class HomeFragment : Fragment() {
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+
+class HomeFragment : Fragment(), OnMapReadyCallback {
+
+    private var mMapView: MapView? = null
+
+    private val MAPVIEW_BUNDLE_KEY = "MapViewBundleKey"
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         (requireActivity() as AppCompatActivity).supportActionBar?.hide()
@@ -25,8 +37,72 @@ class HomeFragment : Fragment() {
             findNavController().navigate(R.id.action_homeFragment_to_searchBeerFragment)
         }
 
+        var mapViewBundle: Bundle? = null
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY)
+        }
+        mMapView = view.findViewById(R.id.googleMap) as MapView
+        mMapView!!.onCreate(mapViewBundle)
+
+        mMapView!!.getMapAsync(this)
+
 
         return view
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        var mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY)
+        if (mapViewBundle == null) {
+            mapViewBundle = Bundle()
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle)
+        }
+        mMapView!!.onSaveInstanceState(mapViewBundle)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mMapView!!.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mMapView!!.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mMapView!!.onStop()
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        map.isMyLocationEnabled = true
+    }
+
+    override fun onPause() {
+        mMapView!!.onPause()
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        mMapView!!.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mMapView!!.onLowMemory()
     }
 
 }
